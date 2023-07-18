80aab2b3ba7a3373de22621bf404c939ffb0eb4e
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2023-07-18 18:30:52 +0530

    Create optuna.py

---
+++ optuna.py
@@ -0,0 +1,28 @@
+def objective(trial):
+    """Define the objective function"""
+    
+    # parameters can be changed subject to the model used
+    params = {
+        'max_depth': trial.suggest_int('max_depth', 1, 9),
+        'learning_rate': trial.suggest_loguniform('learning_rate', 0.01, 1.0),
+        'n_estimators': trial.suggest_int('n_estimators', 50, 500),
+        'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
+        'gamma': trial.suggest_loguniform('gamma', 1e-8, 1.0),
+        'subsample': trial.suggest_loguniform('subsample', 0.01, 1.0),
+        'colsample_bytree': trial.suggest_loguniform('colsample_bytree', 0.01, 1.0),
+        'reg_alpha': trial.suggest_loguniform('reg_alpha', 1e-8, 1.0),
+        'reg_lambda': trial.suggest_loguniform('reg_lambda', 1e-8, 1.0),
+        'eval_metric': 'mlogloss',
+        'use_label_encoder': False
+    }
+
+    # Fit the model
+    optuna_model = XGBClassifier(**params)
+    optuna_model.fit(X_train, y_train)
+
+    # Make predictions
+    y_pred = optuna_model.predict(X_test)
+
+    # Evaluate predictions
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
