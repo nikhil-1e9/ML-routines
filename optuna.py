c106e73b0cb5bfa6e96e31c2ce4df30aa8248af0
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2023-07-24 01:03:09 +0530

    Update optuna.py

--- optuna.py
+++ optuna.py
@@ -8,14 +8,14 @@ def objective(trial):
     # parameters can be changed subject to the model used
     params = {
         'max_depth': trial.suggest_int('max_depth', 1, 9),
-        'learning_rate': trial.suggest_loguniform('learning_rate', 0.01, 1.0),
+        'learning_rate': trial.suggest_float('learning_rate', 0.01, 1.0),
         'n_estimators': trial.suggest_int('n_estimators', 50, 500),
         'min_child_weight': trial.suggest_int('min_child_weight', 1, 10),
-        'gamma': trial.suggest_loguniform('gamma', 1e-8, 1.0),
-        'subsample': trial.suggest_loguniform('subsample', 0.01, 1.0),
-        'colsample_bytree': trial.suggest_loguniform('colsample_bytree', 0.01, 1.0),
-        'reg_alpha': trial.suggest_loguniform('reg_alpha', 1e-8, 1.0),
-        'reg_lambda': trial.suggest_loguniform('reg_lambda', 1e-8, 1.0),
+        'gamma': trial.suggest_float('gamma', 1e-8, 1.0),
+        'subsample': trial.suggest_float('subsample', 0.01, 1.0),
+        'colsample_bytree': trial.suggest_float('colsample_bytree', 0.01, 1.0),
+        'reg_alpha': trial.suggest_float('reg_alpha', 1e-8, 1.0),
+        'reg_lambda': trial.suggest_float('reg_lambda', 1e-8, 1.0),
         'eval_metric': 'auc',
         'use_label_encoder': False
     }
@@ -31,15 +31,11 @@ def objective(trial):
     accuracy = accuracy_score(y_test, y_pred)
     return accuracy
 
+# direction depends on the evaluation metric
 study = optuna.create_study(direction='maximize')
 study.optimize(objective, n_trials=100)
 
 print('Number of finished trials: {}'.format(len(study.trials)))
-print('Best trial:')
 trial = study.best_trial
-
-print('  Value: {}'.format(trial.value))
-print('  Params: ')
-
-for key, value in trial.params.items():
-    print('    {}: {}'.format(key, value))
+print('Best trial value:', trial.value)
+print('Best Params:', study.best_params)
